1456a5c9734abc261de9fc038b4834719a505420
Yannik Sembritzki <yannik@sembritzki.me>
2016-03-28 13:58:38 +0200

    Improve rsnapshot-run.sh

--- templates/rsnapshot-run.sh
+++ templates/rsnapshot-run.sh
@@ -20,11 +20,13 @@ function log_error {
 log_info "Starting ${0}..."
 # Sleep a few seconds to not collide with rotating cronjobs starting at the full minute too
 sleep 3
+failedjob=0
 
 {% for backup in rsnapshot_backups %}
 {% if backup.enabled|default(True) %}
 intervalint={{ backup.interval | replace('every', '') | regex_replace ('[^0-9]*', '') }}
 intervalunit="{{ backup.interval | replace('every', '') | regex_replace('[0-9]*', '') }}"
+
 if [ "$intervalunit" == "min" ]; then
   let intervalsecs="$intervalint * 60"
 elif [ "$intervalunit" == "h" ]; then
@@ -32,35 +34,38 @@ elif [ "$intervalunit" == "h" ]; then
 fi
 
 lastsync_file="/etc/rsnapshot/rsnapshot-{{ backup.name }}.lastsuccess"
+
 if [ -f "$lastsync_file" ]; then
   lastsync=$(cat ${lastsync_file})
 else
   lastsync=0
 fi
+
 currtime=$(date +%s)
 let timediff="$currtime - $lastsync + 5"
-synctime=$(date +%s)
+
 if [ "$timediff" -gt "$intervalsecs" ]; then
-  log_info "Attempting sync for {{ backup.name }}"
   pidfile="/var/run/rsnapshot-{{ backup.name }}.pid"
   sleeptime=0
   maxsleeptime=60
+
+  log_info "Attempting sync for {{ backup.name }}"
+
   while [ -f "$pidfile" ] && kill -0 $(cat "$pidfile") && [ $sleeptime -lt $maxsleeptime ]; do
     log_info "rsnapshot for {{ backup.name }} is already running (probably rotating)"
-    sleep 5
     let sleeptime="$sleeptime + 5"
+    sleep 5
   done
+
   log_info "Running sync for {{ backup.name }}"
   if ! rsnapshot -c /etc/rsnapshot/rsnapshot-{{ backup.name }}.conf sync; then
     log_error "Failed backup {{ backup.name }}"
-    failedjobs=1
+    failedjob=1
   else
-    echo "$synctime" > "$lastsync_file"
+    echo "$currtime" > "$lastsync_file"
     log_info "Successfully synced {{ backup.name }}"
   fi
 fi
 {% endif %}
 {% endfor %}
-if [ "$failedjobs" == 1 ]; then
-  exit 1
-fi
+exit "$failedjob"
